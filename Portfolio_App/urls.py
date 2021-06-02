912cd8d3077707b6d890fc7668d18cd3994988d8
Zahed75 <Zahedhasan.CS@gmail.com>
2021-06-02 13:49:19 +0600

    mail error

--- Portfolio_App/urls.py
+++ Portfolio_App/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from django.urls import path
+
 from Portfolio_App import views
+
 
 app_name='Portfolio_App'
 
 urlpatterns = [
     path('',views.index,name='index'),
+   
+   
 ]
--- Portfolio_App/views.py
+++ Portfolio_App/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render,HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect
 from django.http import HttpResponse
-from Portfolio_App.models import herohome,about,service,experience,banner,portfolio,discount,plan,client,skillname
+from django.urls import reverse
+from Portfolio_App.models import herohome, about, service, experience, banner, portfolio, discount, plan, client, skillname
 
 # Create your views here.
 
+
 def index(request):
-    header=herohome.objects.all()
-    skills=about.objects.all()
-    skilltitle=skillname.objects.all()
-    services=service.objects.all()
-    context={'header':header,'skills':skills,'skilltitle':skilltitle,'services':services}
-    return render(request,'Portfolio_App/index.html',context)
+
+
+    header = herohome.objects.all()
+    skills = about.objects.all()
+    skilltitle = skillname.objects.all()
+    services = service.objects.all()
+    context = {'header': header, 'skills': skills,
+               'skilltitle': skilltitle, 'services': services}
+    return render(request, 'Portfolio_App/index.html', context)
+
+
+
+
